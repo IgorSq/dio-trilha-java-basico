c4e1a125f1722bae6350ca2d2c1d184748ccc7a4
IgorSq <igor.quintiliano.silva@gmail.com>
2025-02-13 13:35:02 -0300

    Operadores Java

--- TiposVariaveis/src/Operadores.java
+++ TiposVariaveis/src/Operadores.java
@@ -1,11 +1,43 @@
 public class Operadores {
     public static void main(String[] args ) {
         
-        String nomeCompleto = "LINGUAGEM" + "JAVA";
+        int numero = 5;
 
-        System.out.println(nomeCompleto);
+        numero ++;
 
+        System.out.println(numero);
+
+        int a, b;
+        a = 5;
+        b = 5;
+
+        String resultado = a==b ? "Verdadeiro" : "falso";
+
+        System.out.println(resultado);
         
+        if(a > b){
+            System.out.println("Primeiro numero maior que o segundo");
+        } 
+        
+        else if (b > a) {
+            System.out.println("Segundo numero maior que o primeiro");
+        }
+        else {
+            System.out.println("OS DOIS SÃO IGUAIS");
+        }
+
+        boolean condicao1 = true;
+
+        boolean condicao2 = false;
+
+        if (condicao1 || condicao2){
+System.out.println("UMA DAS CONDIÇÕES É VERDADEIRA");
+        }
+
+        if (condicao1 && condicao2) {
+            System.out.println("AS DUAS SÃO VERDADEIRAS");
+
+        }
 
     }
 }
--- TiposVariaveis/src/TiposVariaveis.java
+++ TiposVariaveis/src/TiposVariaveis.java
@@ -1,18 +1,23 @@
 public class TiposVariaveis {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
+
     double salarioMinimo = 2500;
+
+    System.out.println(salarioMinimo);
 
     short numeroCurto = 1;
     int numeroNormal = numeroCurto;
     short numeroCurto2 = (short) numeroNormal;
 
-    int numero = 1;
+    System.out.println(numeroCurto2);
 
-    numero = 10;
+    int numero = 1;
 
     System.out.println(numero);
 
     final double VALOR_DE_PI = 3.14;
+
+    System.out.println(VALOR_DE_PI);
 
 
     }
